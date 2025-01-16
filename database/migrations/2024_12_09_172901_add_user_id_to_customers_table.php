cea33f64fc395ac0f88173e9ff8f0a84c867bfcc
hafidzfadillah23@gmail.com <hafidzfadillah23@gmail.com>
2025-01-16 18:39:54 +0700

    pre-deploy: fix migration user id to tb_cust

--- database/migrations/2024_12_09_172901_add_user_id_to_customers_table.php
+++ database/migrations/2024_12_09_172901_add_user_id_to_customers_table.php
@@ -12,12 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('tb_customer', function (Blueprint $table) {
-            $table->id('cust_id'); // Primary key
-            $table->string('cust_name', 255);
-            $table->string('cust_email', 255)->unique();
-            $table->string('cust_password', 255);
-            $table->string('cust_nohp', 255);
-            $table->timestamp('created_at')->useCurrent();
+            // $table->id('cust_id'); // Primary key
+            // $table->string('cust_name', 255);
+            // $table->string('cust_email', 255)->unique();
+            // $table->string('cust_password', 255);
+            // $table->string('cust_nohp', 255);
+            // $table->timestamp('created_at')->useCurrent();
             $table->unsignedBigInteger('user_id')->nullable();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
@@ -29,6 +29,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tb_customer');
+        Schema::table('tb_customer', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+        });
     }
 };
